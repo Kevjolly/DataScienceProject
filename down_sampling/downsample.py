1204d61f107cc2e1b661eb8b9aeb0f044dda55fd
mattclifford2 <mcliffoes@gmail.com>
2019-04-03 12:40:48 +0100

    fft

--- down_sampling/downsample.py
+++ down_sampling/downsample.py
@@ -3,10 +3,18 @@ import sys
 sys.path.append('../')    # import from folder one directory out
 import util
 import matplotlib.pyplot as plt
+import pandas as pd
 
 
-# divergance and information measures
+def frequency_dist(time_series, original_len):
+    ft = np.fft.rfft(time_series)
+    dist = np.zeros(int((original_len/2)+1))
+    dist[:len(ft)] = abs(ft)  # pad with zeros of higher frequencies that can't be captured
+    return dist/dist.sum()# make to distribution
+
+
 def get_KL_div(p, q):   # p and q distributions (ie non-negative)
+    # TODO: symetric KL
     # add a small value to avoid dividings by zero
     e = 2**-100
     norm_p = (p+e)/(p+e).sum()
@@ -14,37 +22,113 @@ def get_KL_div(p, q):   # p and q distributions (ie non-negative)
     return (norm_p*np.log2(norm_p / norm_q)).sum()
 
 
-def est_dist_hist(np_period, min_max):
+def est_dist_hist(np_period, min_max, bins):
     (df_min, df_max) = min_max
-    [count, bin_lower]  = np.histogram(np_period, bins=5000, range=(np.floor(df_min), np.ceil(df_max)))
+    [count, bin_lower]  = np.histogram(np_period, bins=bins, range=(np.floor(df_min), np.ceil(df_max)))
     return count/count.sum()    # normalise to distribution
 
 
-def get_down_sample(original, reduction):
+def even_down_sample(original, reduction):
     original_samples = len(original)
     indicies = np.linspace(0, original_samples-1, int(original_samples/reduction)).astype(int)
     return original[indicies]
 
 
-def run_down_sample(original, reductions, min_max):    # input numpy array
-    original_hist = est_dist_hist(original, min_max)
-    KL_div = []
+def run_down_sample_hist(original, reductions, min_max, bins):    # input numpy array
+    original_len = len(original)
+    original_hist = est_dist_hist(original, min_max, bins)
+    KL_div_hist = []
     for reduction in reductions:
-        down_hist = est_dist_hist(get_down_sample(original, reduction), min_max)
-        KL_div.append(get_KL_div(original_hist, down_hist))
-    return np.array(KL_div)
+        even_down = even_down_sample(original, reduction)
+        
+        down_hist = est_dist_hist(even_down, min_max, bins)
+
+        KL_div_hist.append(get_KL_div(original_hist, down_hist))
+    return np.array(KL_div_hist)
 
 
-def main(df, upper_down):
+def run_down_sample_fft(original, reductions, min_max, bins):    # input numpy array
+    original_len = len(original)
+    original_fft = frequency_dist(original, original_len)
+    KL_div_fft = []
+    for reduction in reductions:
+        even_down = even_down_sample(original, reduction)
+        
+        down_fft = frequency_dist(even_down, original_len)
+
+        KL_div_fft.append(get_KL_div(original_fft, down_fft))
+    return np.array(KL_div_fft)
+
+
+def plot_down_sampled(original, reductions):
+    fig = plt.figure()
+    plot_num = 1
+    print(original.shape)
+    for reduction in reductions:
+        even_down = even_down_sample(original, reduction)
+        plt.subplot(len(reductions), 1, plot_num)
+        plt.plot(np.linspace(0, 1, len(even_down)), even_down)
+        plot_num += 1
+    plt.show()
+
+
+def get_mean(df, reductions, run_function, bins):
     min_max = (df.min().min(), df.max().max())
-    reductions = np.linspace(1, upper_down, 20)
+    
     mean_KL = np.zeros(len(reductions))
     count = 0
     for index, row in df.iterrows():
-        mean_KL += run_down_sample(row.values, reductions, min_max)
+        mean_KL += run_function(row.values, reductions, min_max, bins)
         count += 1
-    mean_KL /= count
-    plt.plot(reductions, mean_KL)
+    return mean_KL / count
+
+
+def downsampled_df(df, reduction):
+    down_df = [0]*df.shape[0]
+    count = 0
+    for index, row in df.iterrows():
+        down_df[count] = even_down_sample(row.values, reduction)
+        count += 1
+    return pd.DataFrame(np.array(down_df))
+
+
+def get_distances(df, reductions):
+    means = []
+    stds = []
+    for reduction in reductions:
+        down_df = downsampled_df(df, reduction)
+        class_mean, class_std = util.mean_std_of_class(down_df)
+        means.append(np.mean(class_mean))
+        stds.append(np.mean(class_std))
+    return (means, stds)
+
+
+def test_distances(df, reductions):
+    classes = util.unique_classes(df)
+    fig = plt.figure()
+    plot_num = 1
+    for star_type in classes:
+        all_class = df.loc[[star_type]]
+        (means, stds) = get_distances(all_class, reductions)
+        plt.subplot(len(classes), 1, plot_num)
+        plt.plot(reductions, means, label = 'mean class: '+str(star_type))
+        # plt.plot(reductions, stds, label = 'std class: '+str(star_type))
+        plt.legend()
+        plot_num += 1
+    plt.show()
+
+
+def main(df, upper_down):
+    reductions = np.linspace(1, upper_down, 8)
+    test_distances(df, reductions)
+    plot_down_sampled(df.iloc[0], reductions)
+    bins = [10, 100, 200, 500, 1000]
+    for single_bin in bins:   
+        # current_mean = get_mean(df, reductions, run_down_sample_hist, single_bin)
+        # plt.plot(reductions, current_mean, label ='hist: bin size '+str(single_bin))
+        print(single_bin)
+    plt.plot(reductions, get_mean(df, reductions, run_down_sample_fft, 0), label='fourier transform')
+    plt.legend()
     plt.xlabel('Reduction Size')
     plt.ylabel('KL divergance')
     plt.show()
